613ad5168fc7eadd4c0ffa1928d0df7535a0cb3b
iknow <iknow@DESKTOP-8D9SJL3>
2025-11-06 11:12:01 +0300

    Initial commit to new PaperWork repository

--- PaperWork.Api/obj/Release/net8.0/PaperWork.Api.AssemblyInfo.cs
+++ PaperWork.Api/obj/Release/net8.0/PaperWork.Api.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaperWork.Api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b119535020aecd7f550564333a5d8a8a9d6783d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaperWork.Api")]
